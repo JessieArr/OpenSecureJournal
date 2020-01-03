3038deab562d5929995e7fab77501eca5cdf61d0
ShaWn <smorrison1986@gmail.com>
2020-01-03 02:05:53 -0600

    Initial version of WPF app.

---
+++ OpenSecureJournal.Test/AESThenHMACTests.cs
@@ -0,0 +1,49 @@
+using OpenSecureJournal.Services;
+using System;
+using Xunit;
+
+namespace OpenSecureJournal.Test
+{
+    public class AESThenHMACTests
+    {
+        [Fact]
+        public void SimpleEncryptWithPassword_RightPassword_CanBeDecrypted()
+        {
+            var secret = "test";
+            var password = "my12characterpass";
+            var encryptedText = AESThenHMAC.SimpleEncryptWithPassword(secret, password);
+            var decryptedText = AESThenHMAC.SimpleDecryptWithPassword(encryptedText, password);
+
+            Assert.Equal(secret, decryptedText);
+        }
+
+        [Fact]
+        public void SimpleEncryptWithPassword_WrongPassword_CannotBeDecrypted()
+        {
+            var secret = "test";
+            var password = "my12characterpass";
+            var wrongPassword = "thisisthewrongpass1";
+            var encryptedText = AESThenHMAC.SimpleEncryptWithPassword(secret, password);
+            var decryptedText = AESThenHMAC.SimpleDecryptWithPassword(encryptedText, wrongPassword);
+
+            Assert.NotEqual(secret, decryptedText);
+        }
+
+        [Fact]
+        public void PerformanceTest()
+        {
+            var reallyLongString = "";
+            var password = "my12characterpass";
+
+            for(var i = 0; i < 1000; i++)
+            {
+                reallyLongString += "Lorem ipsum dolor sit amet";
+            }
+
+            var encryptedText = AESThenHMAC.SimpleEncryptWithPassword(reallyLongString, password);
+            var decryptedText = AESThenHMAC.SimpleDecryptWithPassword(encryptedText, password);
+
+            Assert.NotEqual(reallyLongString, decryptedText);
+        }
+    }
+}
---
+++ OpenSecureJournal/Views/AboutPage.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace OpenSecureJournal.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class AboutPage : ContentPage
+    {
+        public AboutPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
